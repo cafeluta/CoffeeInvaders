68f8864a42ec242c380b987d48f95e8d5b7dc58a
cafeluta <sebi.ionita23@gmail.com>
2025-04-18 18:59:16 +0300

    Implemented a Player GameObject and a simple WASD movement
    Signed-off-by: cafeluta <sebi.ionita23@gmail.com>

--- src/Game/game.cpp
+++ src/Game/game.cpp
@@ -2,7 +2,13 @@
 #include "../../include/Engine/sprite_renderer.h"
 #include "../../include/Engine/resource_manager.h"
 
+#include <GLFW/glfw3.h>
+
+const glm::vec2 PLAYER_SIZE(64.0f, 64.0f);
+const float PLAYER_VELOCITY(300.0f);
+
 SpriteRenderer* Renderer;
+GameObject* Player;
 
 Game::Game(GLuint width, GLuint height)
 : State(GAME_ACTIVE), Keys(), Width(width), Height(height) {
@@ -13,7 +19,7 @@ void Game::init() {
     // load shaders
     ResourceManager::loadShader("shaders/shader.vs", "shaders/shader.fs", nullptr, "sprite");
 
-    // load the image texture
+    // load the player texture
     ResourceManager::loadTexture2D("assets/spaceship01.png", true, "spaceship");
 
     // load the background
@@ -38,6 +44,13 @@ void Game::init() {
 
     // render controls
     Renderer = new SpriteRenderer(ResourceManager::getShader("sprite"));  // we need a shader to initialize a new sprite
+
+    // player
+    glm::vec2 playerPosition = glm::vec2(
+        this->Width / 2.0f - PLAYER_SIZE.x / 2.0f,
+        this->Height - PLAYER_SIZE.y
+    );
+    Player = new GameObject(playerPosition, PLAYER_SIZE, ResourceManager::getTexture2D("spaceship"));
 }
 
 void Game::update(float dt) {
@@ -45,7 +58,25 @@ void Game::update(float dt) {
 }
 
 void Game::processInput(float dt) {
-
+    if (this->State == GAME_ACTIVE) {
+        float velocity = PLAYER_VELOCITY * dt;
+        if (this->Keys[GLFW_KEY_D]) {
+            if (Player->Position.x  + velocity <= this->Width - Player->Size.x)
+                Player->Position.x += velocity;
+        }
+        if (this->Keys[GLFW_KEY_A]) {
+            if (Player->Position.x - velocity >= 0.0f)
+                Player->Position.x -= velocity;
+        }
+        if (this->Keys[GLFW_KEY_W]) {
+            if (Player->Position.y - velocity >= 0.0f)
+                Player->Position.y -= velocity;
+        }
+        if (this->Keys[GLFW_KEY_S]) {
+            if (Player->Position.y + velocity <= this->Height - Player->Size.y)
+                Player->Position.y += velocity;
+        }
+    }
 }
 
 void Game::render() {
@@ -57,8 +88,7 @@ void Game::render() {
         this->Levels[this->Level].draw(*Renderer);
 
         // render the spaceship sprite
-        Renderer->drawSprite(ResourceManager::getTexture2D("spaceship"), glm::vec2(550.0f, 300.0f), glm::vec2(64.0f, 64.0f), 45.0f, glm::vec3(1.0f, 1.0f, 1.0f));
-        // TODO make the spaceship the player
+        Player->draw(*Renderer);
     }
 }
 
